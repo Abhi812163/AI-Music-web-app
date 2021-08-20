bf6b54aba1c88ae147dc4b7ccba1add0ae23a0ea
Abhi812163 <76207414+Abhi812163@users.noreply.github.com>
2021-08-20 14:40:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,4 +2,16 @@ function setup(){
     canvas=createCanvas(550,500);
     canvas.position(900,150);
     canvas.center();
+
+    video=createCapture(VIDEO);
+    video.hide();
 }
+
+function draw(){
+    image(video,0,0,600,500);
+}
+
+function preload(){
+    song=loadSound("music1.mp3");
+}
+
